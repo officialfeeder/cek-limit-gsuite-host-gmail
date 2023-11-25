fb75bfaa8793aff66aeb43c00ab8741999e947f5
officialfeeder <62536001+officialfeeder@users.noreply.github.com>
2023-11-26 04:42:55 +0700

    Create versi-spreadsheet.js

---
+++ versi-spreadsheet.js
@@ -0,0 +1,11 @@
+function resendEmail() {
+  console.log(MailApp.getRemainingDailyQuota());
+  // var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1'); 
+  // let category = sheet.getRange(x, x).getValue(); 
+  // let size = sheet.getRange(y,y).getValue();
+  // let name = sheet.getRange(z, z).getValue(); 
+  // let row = sheet.getActiveRange().getRow(); 
+//   const colLimit = 10;
+
+//   var lastRow = sheet.getLastRow();
+}
